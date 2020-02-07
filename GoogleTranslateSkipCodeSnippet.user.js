dcbf1f8cee3513f4d80b6fc2f6fa787f25346d31
n3taway <n3taway@gmail.com>
2020-02-07 15:11:34 +0800

    fix: 🚑 github CSS代码块支持

--- GoogleTranslateSkipCodeSnippet.user.js
+++ GoogleTranslateSkipCodeSnippet.user.js
@@ -16,6 +16,7 @@
         '.gist',
         '.type-powershell',
         '.type-javascript',
+        '.type-css',
     ];
     nodeNames.forEach((name)=>{
        [...document.querySelectorAll(name)].forEach( node => {
